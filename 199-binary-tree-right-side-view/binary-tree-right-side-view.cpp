4efdc039c22e2313171742bb93932a0f3f432ecf
Murt2005 <149847713+Murt2005@users.noreply.github.com>
2025-09-26 12:07:54 -0700

    Time: 0 ms (100.00%) | Memory: 14.8 MB (83.10%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -9,29 +9,53 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        if (root == nullptr) return { };
-        vector<int> result;
-        queue<TreeNode*> q;
-        q.push(root);
-        while (!q.empty()) {
-            int qSize = q.size();
-            for (int s = 0; s < qSize; s++) {
-                auto node = q.front();
-                q.pop();
-                if (node->left) {
-                    q.push(node->left);
-                }
-                if (node->right) {
-                    q.push(node->right);
-                }
-                if (s == qSize - 1) {
-                    result.push_back(node->val);
-                }
-            }
-        }
+        dfs(root, 0);
         return result;
     }
+
+private:
+    vector<int> result;
+
+    void dfs(TreeNode* node, int depth) {
+        if (!node) return;
+
+        if (result.size() == depth) {
+            result.push_back(node->val);
+        }
+
+        dfs(node->right, depth + 1);
+        dfs(node->left, depth + 1);
+    }
 };
+
+
+// class Solution {
+// public:
+//     vector<int> rightSideView(TreeNode* root) {
+//         if (root == nullptr) return { };
+//         vector<int> result;
+//         queue<TreeNode*> q;
+//         q.push(root);
+//         while (!q.empty()) {
+//             int qSize = q.size();
+//             for (int s = 0; s < qSize; s++) {
+//                 auto node = q.front();
+//                 q.pop();
+//                 if (node->left) {
+//                     q.push(node->left);
+//                 }
+//                 if (node->right) {
+//                     q.push(node->right);
+//                 }
+//                 if (s == qSize - 1) {
+//                     result.push_back(node->val);
+//                 }
+//             }
+//         }
+//         return result;
+//     }
+// };
