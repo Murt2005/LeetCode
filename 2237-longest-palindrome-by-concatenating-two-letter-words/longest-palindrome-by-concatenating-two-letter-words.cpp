6f12906dc7314ac279663f6e7e5c331354254b02
Murt2005 <149847713+Murt2005@users.noreply.github.com>
2025-09-30 13:17:15 -0700

    Time: 34 ms (73.11%) | Memory: 171.9 MB (78.45%) - LeetSync

---
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        unordered_map<string, int> wordFreq;
+        for (auto& word : words) {
+            wordFreq[word]++;
+        }
+
+        int longestLength = 0;
+        bool hasMiddle = false;
+
+        for (auto& [word, count] : wordFreq) {
+            string reverse = string{word[1], word[0]};
+            if (word == reverse) {
+                longestLength += (count / 2) * 4;
+                if (count % 2 == 1) hasMiddle = true;
+            } else if (wordFreq.contains(reverse)) {
+                int pairs = min(count, wordFreq[reverse]);
+                longestLength += pairs * 4;
+                wordFreq[reverse] = 0;
+            }
+        }
+        if (hasMiddle) longestLength += 2;
+        return longestLength;
+    }
+};
