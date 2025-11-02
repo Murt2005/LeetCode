99ad82c6874128437fa99f9f4d9ec1d20987caa4
Murt2005 <149847713+Murt2005@users.noreply.github.com>
2025-11-01 18:47:07 -0700

    Time: 20 ms (40.78%) | Memory: 28.4 MB (21.90%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -4,28 +4,58 @@ public:
         vector<vector<string>> result;
         unordered_map<string, vector<string>> mp;
 
-        for (const auto& s: strs) {
-            string temp = s;
-            sort(temp.begin(), temp.end());
-            mp[temp].push_back(s);
+        for (const auto& s : strs) {
+            string key = getKey(s);
+            mp[key].push_back(s);
         }
 
         for (auto& [key, vec] : mp) {
-            result.push_back(vec);
+            result.push_back(std::move(vec));
         }
 
         return result;
     }
+
+private:
+    string getKey(const string& s) {
+        vector<int> count(26,0);
+        
+        for (char c : s) {
+            count[c - 'a']++;
+        }
+
+        string key;
+        for (int i = 0; i < 26; i++) {
+            if (count[i] > 0) {
+                key += string(count[i], i + 'a');
+            }
+        }
+
+        return key;
+    }
 };
+
+
 
 // Time Complexity: O(N * klogk) where k is the max length of a string
 // Space Complexity: O(N)
+//
+// class Solution {
+// public:
+//     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+//         vector<vector<string>> result;
+//         unordered_map<string, vector<string>> mp;
 
+//         for (const auto& s: strs) {
+//             string temp = s;
+//             sort(temp.begin(), temp.end());
+//             mp[temp].push_back(s);
+//         }
 
-/*
+//         for (auto& [key, vec] : mp) {
+//             result.push_back(vec);
+//         }
 
-unordered_map<string, vector<string>> (maps sorted word to the word)
-
-
-
-*/
+//         return result;
+//     }
+// };
