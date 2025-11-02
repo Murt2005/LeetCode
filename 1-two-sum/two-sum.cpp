a5d3b19f75fcbf73b491810d645f261870a5cf4e
Murt2005 <149847713+Murt2005@users.noreply.github.com>
2025-11-01 17:10:53 -0700

    Time: 4 ms (55.32%) | Memory: 15 MB (19.85%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,13 +1,34 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size() - 1; i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
+        unordered_map<int, int> numToIndex;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (numToIndex.find(target - nums[i]) != numToIndex.end()) {
+                return {i, numToIndex[target - nums[i]]};
             }
+            numToIndex[nums[i]] = i;
         }
+
         return {};
     }
 };
+
+
+// BRUTE FORCE SOLUTION
+// TIME COMPLEXITY: O(N^2)
+// SPACE COMPLEXITY: O(1)
+//
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         for (int i = 0; i < nums.size() - 1; i++) {
+//             for (int j = i + 1; j < nums.size(); j++) {
+//                 if (nums[i] + nums[j] == target) {
+//                     return {i, j};
+//                 }
+//             }
+//         }
+//         return {};
+//     }
+// };
