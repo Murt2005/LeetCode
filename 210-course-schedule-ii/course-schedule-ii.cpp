0005e1f12aa1aa77d1b0c830a549f0d643616248
Murt2005 <149847713+Murt2005@users.noreply.github.com>
2025-11-01 19:33:53 -0700

    Time: 8 ms (23.56%) | Memory: 18.4 MB (58.68%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,41 +1,95 @@
 class Solution {
+private:
+    bool isCycle(vector<vector<int>>& adj, vector<int>& visited, int i, vector<int>& result) {
+        if (visited[i] == 1) {
+            return true;
+        }
+
+        if (visited[i] == 2) {
+            return false;
+        }
+
+        visited[i] = 1;
+
+        for (const auto& next : adj[i]) {
+            if (isCycle(adj, visited, next, result)) {
+                return true;
+            }
+        }
+        visited[i] = 2;
+        result.push_back(i);
+        return false;
+    }
+
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
-        vector<int> inDegree(numCourses, 0);
 
         for (const auto& pre : prerequisites) {
             adj[pre[1]].push_back(pre[0]);
-            inDegree[pre[0]]++;
         }
 
-        queue<int> q;
-        for (int i = 0; i < numCourses; i++) {
-            if (inDegree[i] == 0) {
-                q.push(i);
-            }
-        }
-
+        vector<int> visited(numCourses, 0);
         vector<int> result;
-        int processed = 0;
-        while (!q.empty()) {
-            int course = q.front();
-            q.pop();
-            processed++;
-            result.push_back(course);
 
-            for (int next : adj[course]) {
-                inDegree[next]--;
-                if (inDegree[next] == 0) {
-                    q.push(next);
+        for (int i = 0; i < numCourses; i++) {
+            if (visited[i] == 0) {
+                if (isCycle(adj, visited, i, result)) {
+                    return { };
                 }
             }
+            
         }
 
-        if (processed != numCourses) {
-            return {};
-        }
-
+        reverse(result.begin(), result.end());
         return result;
     }
 };
+
+
+
+
+// Time Complexity: O(V + E)
+// Space Complexity: O(V + E)
+
+// class Solution {
+// public:
+//     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+//         vector<vector<int>> adj(numCourses);
+//         vector<int> inDegree(numCourses, 0);
+
+//         for (const auto& pre : prerequisites) {
+//             adj[pre[1]].push_back(pre[0]);
+//             inDegree[pre[0]]++;
+//         }
+
+//         queue<int> q;
+//         for (int i = 0; i < numCourses; i++) {
+//             if (inDegree[i] == 0) {
+//                 q.push(i);
+//             }
+//         }
+
+//         vector<int> result;
+//         int processed = 0;
+//         while (!q.empty()) {
+//             int course = q.front();
+//             q.pop();
+//             processed++;
+//             result.push_back(course);
+
+//             for (int next : adj[course]) {
+//                 inDegree[next]--;
+//                 if (inDegree[next] == 0) {
+//                     q.push(next);
+//                 }
+//             }
+//         }
+
+//         if (processed != numCourses) {
+//             return {};
+//         }
+
+//         return result;
+//     }
+// };
