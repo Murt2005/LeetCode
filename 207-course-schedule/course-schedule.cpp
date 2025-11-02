7f29607615a40351e0c17b2a11a1df9df8581674
Murt2005 <149847713+Murt2005@users.noreply.github.com>
2025-11-01 19:13:10 -0700

    Time: 3 ms (79.28%) | Memory: 19.2 MB (73.65%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,37 +1,74 @@
 class Solution {
-private:
-    bool isCycle(vector<vector<int>>& adj, vector<int>& visited, int i) {
-        if (visited[i] == 1) {
-            return true;
-        }
-
-        if (visited[i] == 0) {
-            visited[i] = 1;
-            for (const auto& next : adj[i]) {
-                if (isCycle(adj, visited, next)) {
-                    return true;
-                }
-            }
-        }
-        visited[i] = 2;
-        return false;
-    }
-
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
+        vector<int> inDegree(numCourses, 0);
+
         for (const auto& pre : prerequisites) {
             adj[pre[1]].push_back(pre[0]);
+            inDegree[pre[0]]++;
         }
 
-        vector<int> visited(numCourses, 0);
-
+        queue<int> q;
         for (int i = 0; i < numCourses; i++) {
-            if (isCycle(adj, visited, i)) {
-                return false;
+            if (inDegree[i] == 0) {
+                q.push(i);
             }
         }
 
-        return true;
+        int processed = 0;
+        while (!q.empty()) {
+            int course = q.front();
+            q.pop();
+            processed++;
+
+            for (int next : adj[course]) {
+                inDegree[next]--;
+                if (inDegree[next] == 0) {
+                    q.push(next);
+                }
+            }
+        }
+
+        return processed == numCourses;
     }
 };
+
+
+// class Solution {
+// private:
+//     bool isCycle(vector<vector<int>>& adj, vector<int>& visited, int i) {
+//         if (visited[i] == 1) {
+//             return true;
+//         }
+
+//         if (visited[i] == 0) {
+//             visited[i] = 1;
+//             for (const auto& next : adj[i]) {
+//                 if (isCycle(adj, visited, next)) {
+//                     return true;
+//                 }
+//             }
+//         }
+//         visited[i] = 2;
+//         return false;
+//     }
+
+// public:
+//     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+//         vector<vector<int>> adj(numCourses);
+//         for (const auto& pre : prerequisites) {
+//             adj[pre[1]].push_back(pre[0]);
+//         }
+
+//         vector<int> visited(numCourses, 0);
+
+//         for (int i = 0; i < numCourses; i++) {
+//             if (isCycle(adj, visited, i)) {
+//                 return false;
+//             }
+//         }
+
+//         return true;
+//     }
+// };
