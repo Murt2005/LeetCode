88f369e2918954850c1ef88df77aa1937f92933c
Murt2005 <149847713+Murt2005@users.noreply.github.com>
2025-11-03 11:10:45 -0800

    Time: 0 ms (100.00%) | Memory: 8.8 MB (87.27%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,27 +1,30 @@
+// Time Complexity: O(N)
+// Space Complexity: O(N)
 class Solution {
 public:
     bool isValid(string s) {
         stack<char> stck;
 
         for (char c : s) {
-            if (closeToOpen.count(c) && !stck.empty() && stck.top() == closeToOpen[c]) {
-                stck.pop();
-            } else if (closeToOpen.count(c) && !stck.empty() && stck.top() != closeToOpen[c]) {
-                return false;
-            } else {
-                stck.push(c);
+            switch (c) {
+                case ')':
+                    if (stck.empty() || stck.top() != '(') return false;
+                    stck.pop();
+                    break;
+                case '}':
+                    if (stck.empty() || stck.top() != '{') return false;
+                    stck.pop();
+                    break;
+                case ']':
+                    if (stck.empty() || stck.top() != '[') return false;
+                    stck.pop();
+                    break;
+                default: stck.push(c);
             }
         }
 
         return stck.empty();
     }
-
-private:
-    unordered_map<char, char> closeToOpen = {
-        {')', '('}
-        , {'}', '{'}
-        , {']', '['}
-    };
 };
 
 /*
