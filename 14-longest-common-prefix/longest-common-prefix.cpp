e85ad7d81bd2bcbd52ba7e6ec96a0a413b757b5f
Murt2005 <149847713+Murt2005@users.noreply.github.com>
2025-11-03 10:54:40 -0800

    Time: 0 ms (100.00%) | Memory: 11.7 MB (95.31%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,31 +1,43 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        sort(strs.begin(), strs.end());
-        string first = strs[0];
-        string last = strs[strs.size() - 1];
+        if (strs.empty()) {
+            return "";
+        }
 
-        string result;
-        for (int i = 0; i < min(first.length(), last.length()); i++) {
-            if (first[i] == last[i]) {
-                result.push_back(first[i]);
-            } else {
-                break;
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0][i];
+
+            for (int j = 1; j < strs.size(); j++) {
+                if (i >= strs[j].length() || c != strs[j][i]) {
+                    return strs[0].substr(0, i);
+                }
             }
         }
 
-        return result;
+        return strs[0];
     }
 };
 
+// Time Complexity: O(NlogN)
+// Space Complexity: O(m) m = length of common prefix
+//
+// class Solution {
+// public:
+//     string longestCommonPrefix(vector<string>& strs) {
+//         sort(strs.begin(), strs.end());
+//         string first = strs[0];
+//         string last = strs[strs.size() - 1];
 
-/*
+//         string result;
+//         for (int i = 0; i < min(first.length(), last.length()); i++) {
+//             if (first[i] == last[i]) {
+//                 result.push_back(first[i]);
+//             } else {
+//                 break;
+//             }
+//         }
 
-["flower", "flow", "flight"]
-
-["flight", "flow", "flower"]
-
-
-
-
-*/
+//         return result;
+//     }
+// };
