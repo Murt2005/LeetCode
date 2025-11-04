e1c8e0c9c58084938ded4a9080f2ed8b7f28e29c
Murt2005 <149847713+Murt2005@users.noreply.github.com>
2025-11-03 21:07:49 -0800

    Time: 0 ms (100.00%) | Memory: 28.2 MB (63.26%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,21 +1,46 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> numCount;
+        int candidate = 0;
+        int count = 0;
 
         for (int num : nums) {
-            numCount[num]++;
-        }
+            if (count == 0) {
+                candidate = num;
+            }
 
-        int result = 0;
-        int resultFreq = 0;
-        for (const auto& [num, count] : numCount) {
-            if (count > resultFreq) {
-                resultFreq = count;
-                result = num;
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
             }
         }
 
-        return result;
+        return candidate;
     }
 };
+
+// Time Complexity: O(N)
+// Space Complexity: O(1)
+//
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         unordered_map<int, int> numCount;
+
+//         for (int num : nums) {
+//             numCount[num]++;
+//         }
+
+//         int result = 0;
+//         int resultFreq = 0;
+//         for (const auto& [num, count] : numCount) {
+//             if (count > resultFreq) {
+//                 resultFreq = count;
+//                 result = num;
+//             }
+//         }
+
+//         return result;
+//     }
+// };
