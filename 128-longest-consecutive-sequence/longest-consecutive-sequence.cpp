61c5ac178502d3984cc1e5fe21d12e45682c41ae
Murt2005 <149847713+Murt2005@users.noreply.github.com>
2025-11-03 18:52:21 -0800

    Time: 86 ms (46.31%) | Memory: 88.7 MB (72.01%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -2,21 +2,23 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         unordered_set<int> lookup;
+
         for (int num : nums) {
             lookup.insert(num);
         }
 
         int maxStreak = 0;
-
         for (int num : lookup) {
-            if (!lookup.contains(num - 1)) {
-                int streak = 1;
-                while (lookup.contains(num + streak)) {
-                    streak++;
-                }
-
-                maxStreak = max(maxStreak, streak);
+            if (lookup.find(num - 1) != lookup.end()) {
+                continue;
             }
+
+            int streak = 1;
+            while (lookup.find(num + streak) != lookup.end()) {
+                streak++;
+            }
+
+            maxStreak = max(maxStreak, streak);
         }
 
         return maxStreak;
@@ -24,10 +26,18 @@ public:
 };
 
 /*
+O(NlogN)
+
+[1,2,3,4,100,200]
+ 
+
+
+O(N)
 
 [100,4,200,1,3,2]
 
-           ^
+put all elements in an unordered_set<>
+
 
 
 
