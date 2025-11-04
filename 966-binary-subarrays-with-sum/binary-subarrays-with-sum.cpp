dbd86ef643394207ffd51f1bd9cd0f8d73ff1292
Murt2005 <149847713+Murt2005@users.noreply.github.com>
2025-11-04 12:57:35 -0800

    Time: 1 ms (61.73%) | Memory: 32.7 MB (59.39%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,25 +1,57 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int totalSum = 0;
+        return helper(nums, goal) - helper(nums, goal - 1);
+    }
+
+private:
+    int helper(vector<int>& nums, int goal) {
+        int start = 0;
+        int currentSum = 0;
         int result = 0;
 
-        unordered_map<int, int> freq;
+        for (int end = 0; end < nums.size(); end++) {
+            currentSum += nums[end];
 
-        for (int num : nums) {
-            totalSum += num;
-            
-            if (totalSum == goal) {
-                result++;
+            while (start <= end && currentSum > goal) {
+                currentSum -= nums[start];
+                start++;
             }
 
-            if (freq.find(totalSum - goal) != freq.end()) {
-                result += freq[totalSum - goal];
-            }
-
-            freq[totalSum]++;
+            result += end - start + 1;
         }
 
         return result;
     }
 };
+
+
+
+// Time Complexity: O(N)
+// Space Complexity: O(N)
+//
+// class Solution {
+// public:
+//     int numSubarraysWithSum(vector<int>& nums, int goal) {
+//         int totalSum = 0;
+//         int result = 0;
+
+//         unordered_map<int, int> freq;
+
+//         for (int num : nums) {
+//             totalSum += num;
+            
+//             if (totalSum == goal) {
+//                 result++;
+//             }
+
+//             if (freq.find(totalSum - goal) != freq.end()) {
+//                 result += freq[totalSum - goal];
+//             }
+
+//             freq[totalSum]++;
+//         }
+
+//         return result;
+//     }
+// };
