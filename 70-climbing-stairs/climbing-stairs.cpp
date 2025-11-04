83ce1657730473b260683e6b6060edbac5f9c807
Murt2005 <149847713+Murt2005@users.noreply.github.com>
2025-11-03 19:10:21 -0800

    Time: 0 ms (100.00%) | Memory: 7.8 MB (86.77%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -5,24 +5,37 @@ public:
             return n;
         }
 
-        vector<int> steps(n + 1);
-        steps[n - 1] = 1;
-        steps[n - 2] = 2;
+        int step1 = 1;
+        int step2 = 2;
 
         for (int step = n - 3; step >= 0; step--) {
-            steps[step] = steps[step + 1] + steps[step + 2];
+            int currentStep = step1 + step2;
+            step1 = step2;
+            step2 = currentStep;
         }
 
-        return steps[0];
+        return step2;
     }
 };
 
-/*
+// Time Complexity: O(N)
+// Space Complexity: O(N)
+//
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         if (n <= 2) {
+//             return n;
+//         }
 
+//         vector<int> steps(n + 1);
+//         steps[n - 1] = 1;
+//         steps[n - 2] = 2;
 
-n = 2
+//         for (int step = n - 3; step >= 0; step--) {
+//             steps[step] = steps[step + 1] + steps[step + 2];
+//         }
 
-0 1 2 
-
-
-*/
+//         return steps[0];
+//     }
+// };
