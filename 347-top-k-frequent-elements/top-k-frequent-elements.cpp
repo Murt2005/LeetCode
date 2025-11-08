7f86516f65a312d84052b9f208fe383777367b99
Murt2005 <149847713+Murt2005@users.noreply.github.com>
2025-11-08 09:50:29 -0800

    Time: 4 ms (34.16%) | Memory: 18 MB (37.42%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,31 +1,59 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> freqCount;
-        for (int num : nums) {
-            freqCount[num]++;
-        }
-
-        auto cmp = [](pair<int, int>& a, pair<int, int>& b) {
-            return a.first > b.first;
+        unordered_map<int, int> freqMap; // val -> freq
+        
+        
+        auto cmp = [](const auto& a, const auto& b) {
+            return a.second > b.second;
         };
-
+        
         priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp)> minHeap(cmp);
 
-        for (const auto& [num, freq] : freqCount) {
-            minHeap.push({freq, num});
+        for (int num : nums) {
+            freqMap[num]++;
+        }        
+
+        vector<int> result;
+
+        for (const auto& pair : freqMap) {
+            minHeap.push(pair);
             if (minHeap.size() > k) {
                 minHeap.pop();
             }
         }
 
-        vector<int> result;
-        
         while (!minHeap.empty()) {
-            result.push_back(minHeap.top().second);
+            auto pair = minHeap.top();
             minHeap.pop();
+            int val = pair.first;
+            result.push_back(val);
         }
 
         return result;
     }
 };
+
+/*
+
+given: arr of ints and an int k
+
+return:return k most frequent elements, can be in any order
+
+ds:
+- unordered_map<int, int> val -> freq
+- priority_queue (minHeap)
+
+algorithm:
+- iterate through nums arr
+- build the freqMap
+- for each pair in freq map
+- then iterate through freqMap and put vals in minHeap
+- maintain that the minHeap is at most size k
+- if minHeap is bigger than k, we pop the top val
+- after iterating through freqMap
+- pop all values from minHeap, this will be our result
+
+
+
+*/
