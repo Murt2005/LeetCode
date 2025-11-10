c391a6d7ee362a527818d601964af6831f34c822
Murt2005 <149847713+Murt2005@users.noreply.github.com>
2025-11-09 16:10:00 -0800

    Time: 25 ms (72.13%) | Memory: 16.2 MB (78.81%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -8,7 +8,7 @@ public:
         for (int row = 0; row < ROWS; row++) {
             for (int col = 0; col < COLS; col++) {
                 if (grid[row][col] == '1') {
-                    bfs(grid, row, col);
+                    dfs(grid, row, col);
                     result++;
                 }
             }
@@ -22,27 +22,14 @@ private:
     int COLS;
     int directions[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
 
-    void bfs(vector<vector<char>>& grid, int row, int col) {
-        queue<pair<int, int>> q;
-        q.push({row, col});
+    void dfs(vector<vector<char>>& grid, int row, int col) {
+        grid[row][col] = '0';
+        for (int d = 0; d < 4; d++) {
+            int newRow = row + directions[d][0];
+            int newCol = col + directions[d][1];
 
-        while (!q.empty()) {
-            int qSize = q.size();
-            for (int i = 0; i < qSize; i++) {
-                auto cell = q.front();
-                q.pop();
-                int x = cell.first;
-                int y = cell.second;
-
-                for (int d = 0; d < 4; d++) {
-                    int newX = x + directions[d][0];
-                    int newY = y + directions[d][1];
-
-                    if (newX >= 0 && newY >= 0 && newX < ROWS && newY < COLS && grid[newX][newY] == '1') {
-                        q.push({newX, newY});
-                        grid[newX][newY] = '0';
-                    }
-                }
+            if (newRow >= 0 && newCol >= 0 && newRow < ROWS && newCol < COLS && grid[newRow][newCol] != '0') {
+                dfs(grid, newRow, newCol);
             }
         }
     }
