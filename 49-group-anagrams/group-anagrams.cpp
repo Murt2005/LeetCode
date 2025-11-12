7f1cd6724272b71155f70a9f6367bbe2cee39798
Murt2005 <149847713+Murt2005@users.noreply.github.com>
2025-11-12 14:53:54 -0800

    Time: 19 ms (48.83%) | Memory: 25 MB (77.67%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -2,60 +2,32 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> result;
-        unordered_map<string, vector<string>> mp;
+        unordered_map<string, vector<string>> anagrams;
 
-        for (const auto& s : strs) {
-            string key = getKey(s);
-            mp[key].push_back(s);
+        for (string s : strs) {
+            string temp = s;
+            sort(s.begin(), s.end());
+            anagrams[s].push_back(temp);
         }
 
-        for (auto& [key, vec] : mp) {
-            result.push_back(std::move(vec));
+        for (const auto& [sorted, strings] : anagrams) {
+            result.push_back(strings);
         }
 
         return result;
-    }
-
-private:
-    string getKey(const string& s) {
-        vector<int> count(26,0);
-        
-        for (char c : s) {
-            count[c - 'a']++;
-        }
-
-        string key;
-        for (int i = 0; i < 26; i++) {
-            if (count[i] > 0) {
-                key += string(count[i], i + 'a');
-            }
-        }
-
-        return key;
     }
 };
 
 
 
-// Time Complexity: O(N * klogk) where k is the max length of a string
-// Space Complexity: O(N)
-//
-// class Solution {
-// public:
-//     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-//         vector<vector<string>> result;
-//         unordered_map<string, vector<string>> mp;
+/*
 
-//         for (const auto& s: strs) {
-//             string temp = s;
-//             sort(temp.begin(), temp.end());
-//             mp[temp].push_back(s);
-//         }
+["eat","tea","tan","ate","nat","bat"]
 
-//         for (auto& [key, vec] : mp) {
-//             result.push_back(vec);
-//         }
 
-//         return result;
-//     }
-// };
+algorithm:
+- use an unordered_map<string, vector<string>> anagrams which maps ordered string to the possible anagrams from ordered string
+- loop through strs and sort each strs[i] and assign anagrams[sorted(strs[i])].push_back(strs[i])
+- then loop through the map and put the result into vector<vector<string>> output
+
+*/
