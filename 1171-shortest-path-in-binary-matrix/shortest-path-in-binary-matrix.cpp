8fb48c71d14dd5a46efa376f69fe42923ee38e69
Murt2005 <149847713+Murt2005@users.noreply.github.com>
2025-11-14 11:29:45 -0800

    Time: 16 ms (68.24%) | Memory: 23.8 MB (77.14%) - LeetSync

---
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int ROWS = grid.size();
+        int COLS = grid[0].size();
+
+        if (grid[0][0] == 1) {
+            return -1;
+        }
+
+        if (ROWS == 1 && COLS == 1 && grid[0][0] == 0) {
+            return 1;
+        }
+
+        vector<vector<bool>> visited(ROWS, vector<bool>(COLS, false));
+        queue<pair<int, int>> q;
+        q.push({0,0});
+        int shortestPath = 0;
+
+        while (!q.empty()) {
+            int qSize = q.size();
+            for (int s = 0; s < qSize; s++) {
+                auto cell = q.front();
+                q.pop();
+                int x = cell.first;
+                int y = cell.second;
+
+                if (x == ROWS - 1 && y == COLS - 1 && grid[x][y] == 0) {
+                    return shortestPath + 1;
+                }
+
+                visited[x][y] = true;
+
+                for (int d = 0; d < 8; d++) {
+                    int newX = x + directions[d][0];
+                    int newY = y + directions[d][1];
+
+                    if (newX >= 0 && newY >= 0 && newX < ROWS && newY < COLS && !visited[newX][newY] && grid[newX][newY] == 0) {
+                        q.push({newX, newY});
+                        visited[newX][newY] = true;
+                    }
+                }
+            }
+            shortestPath++;
+        }
+
+        return -1;
+    }
+
+private:
+    int directions[8][2] = {{1,0},{1,1},{0,1},{-1,1},{-1,0},{-1,-1},{0,-1},{1,-1}};
+};
