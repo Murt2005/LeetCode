c0f9ab33ccb18bda5044f849d0ddab71b382fa47
Murt2005 <149847713+Murt2005@users.noreply.github.com>
2026-01-11 17:48:15 -0800

    Time: 0 ms (100.00%) | Memory: 13.7 MB (88.14%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
@@ -1,23 +1,11 @@
 class Solution {
 public:
     int minTimeToVisitAllPoints(vector<vector<int>>& points) {
-        int n = points.size();
         int seconds = 0;
 
-        for (int i = 0; i < n - 1; i++) {
-            auto currentPointCords = points[i];
-            int currentX = currentPointCords[0];
-            int currentY = currentPointCords[1];
-
-            auto nextPointCords = points[i + 1];
-            int nextX = nextPointCords[0];
-            int nextY = nextPointCords[1];
-
-            int xDistance = currentX - nextX;
-            int yDistance = currentY - nextY;
-
-            int dx = abs(nextX - currentX);
-            int dy = abs(nextY - currentY);
+        for (int i = 0; i < points.size() - 1; i++) {
+            int dx = abs(points[i + 1][0] - points[i][0]);
+            int dy = abs(points[i + 1][1] - points[i][1]);
 
             seconds += max(dx, dy);
         }
